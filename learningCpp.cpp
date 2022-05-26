e1999104d2d8338c1bf1195cf1d98d25dbe2f163
Rytnix <utkarshrock1510@gmail.com>
2022-05-26 11:53:12 +0530

    Adding new files

---
+++ learningCpp.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+static void doNothing(int &x) {
+
+}
+
+
+int main() {
+
+
+	int a;
+
+	doNothing(a);
+	cout << a << endl;
+	return 0;
+
+}
+
+
+
+
+
+
